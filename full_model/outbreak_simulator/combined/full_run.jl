26d4ada374e41f70a278dfb0fc29fe5f2b0be187
Cameron Zachreson <cameron.zachreson@gmail.com>
2023-10-17 12:18:49 +1100

    ran first pass of experiment
    varied testing strategy and lockdown efficacy

---
+++ full_model/outbreak_simulator/combined/full_run.jl
@@ -0,0 +1,33 @@
+#include("./Main_RACF_OB_v9.jl")
+
+include("./header_RACF.jl")
+
+include("./Setup_RACF_v9.jl")
+import .Setup_RACF
+
+include("./Networks_RACF_v9.jl")
+import .Networks_RACF
+
+include("./Diseases_RACF_v9.jl")
+import .Diseases_RACF
+
+include("./Agents_RACF_v9.jl")
+import .Agents_RACF
+
+include("./Facility_Structure_v9.jl")
+import .Facility_Structure
+
+include("./Outbreak_Response_RACF_v9.jl")
+import .Outbreak_Response
+
+include("./Transmission_Dynamics_v9.jl")
+import .Transmission_Dynamics
+
+
+
+include("./Main_RACF_OB_v9.jl")
+
+main_OB()
+
+
+
